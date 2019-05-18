a9ad1edce3cbe35ba33f37e345cb0859a10347f1
Siegrift <e.tesarr@gmail.com>
2019-05-18 16:21:28 +0200

    Create apply function

--- src/tsfunct.test.ts
+++ src/tsfunct.test.ts
@@ -1,5 +1,21 @@
-import { fn } from './tsfunct'
+import F from './tsfunct'
+interface Test {
+  a: boolean
+  b: number
+  c: {
+    d: string;
+  }
+}
 
-test('basic', () => {
-  expect(fn(4)).toBe(8)
+test('create tsfunct class from value and get raw value with same type', () => {
+  const obj: Test = { a: true, b: 1, c: { d: 'str' } }
+  // verify that type is correct
+  const value: Test = F(obj).value
+  expect(value).toBe(obj)
+})
+
+test('maps the readonly value to anything', () => {
+  const obj: Test = { a: true, b: 1, c: { d: 'str' } }
+  const value: number = F(obj).apply((val) => val.b + 10).value
+  expect(value).toBe(11)
 })
--- src/tsfunct.ts
+++ src/tsfunct.ts
@@ -1,1 +1,20 @@
-export const fn = (x: number) => x * 2
+export type Primitive = string | number | boolean | undefined | null
+export type DeepReadonly<T> = T extends Primitive ? T : DeepReadonlyObject<T>
+export type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>
+}
+
+export class TsFunct<T> {
+  constructor(private _value: T) {}
+
+  get value() {
+    return this._value
+  }
+
+  apply<NewType>(fn: (value: DeepReadonly<T>) => NewType): TsFunct<NewType> {
+    return createTsFunctInstance(fn(this._value as DeepReadonly<T>))
+  }
+}
+
+const createTsFunctInstance = <T>(value: T) => new TsFunct(value)
+export default createTsFunctInstance
