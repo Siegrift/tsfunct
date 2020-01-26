6243d3f6210cabfadf519877e7625a32f7257e53
Emanuel Tesar <e.tesarr@gmail.com>
2020-01-26 11:34:27 +0100

    Fp helpers for composable logic

--- src/fpUnset/fpUnset.ts
+++ src/fpUnset/fpUnset.ts
@@ -7,171 +7,164 @@ import {
 } from '../common/types'
 import baseUnset from '../common/baseUnset'
 
-type Unset1<T, K1> = Without<T, K1>
-type Unset2<T, K1, K2> = {
-  [P in keyof T]: P extends K1 ? Unset1<T[P], K2> : T[P]
+type FpUnset1<T, K1> = Without<T, K1>
+type FpUnset2<T, K1, K2> = {
+  [P in keyof T]: P extends K1 ? FpUnset1<T[P], K2> : T[P]
 }
-type Unset3<T, K1, K2, K3> = {
-  [P in keyof T]: P extends K1 ? Unset2<T[P], K2, K3> : T[P]
+type FpUnset3<T, K1, K2, K3> = {
+  [P in keyof T]: P extends K1 ? FpUnset2<T[P], K2, K3> : T[P]
 }
-type Unset4<T, K1, K2, K3, K4> = {
-  [P in keyof T]: P extends K1 ? Unset3<T[P], K2, K3, K4> : T[P]
+type FpUnset4<T, K1, K2, K3, K4> = {
+  [P in keyof T]: P extends K1 ? FpUnset3<T[P], K2, K3, K4> : T[P]
 }
-type Unset5<T, K1, K2, K3, K4, K5> = {
-  [P in keyof T]: P extends K1 ? Unset4<T[P], K2, K3, K4, K5> : T[P]
+type FpUnset5<T, K1, K2, K3, K4, K5> = {
+  [P in keyof T]: P extends K1 ? FpUnset4<T[P], K2, K3, K4, K5> : T[P]
 }
 
-interface UnsetFn {
-  <T, K1 extends keyof T>(source: T, path: [K1]): Unset1<T, K1>
-  <T, K1 extends keyof T, K2 extends keyof U<T[K1]>>(
+interface FpUnsetFnReturn<T> {
+  <K1 extends keyof T>(path: [K1]): (source: T) => FpUnset1<T, K1>
+
+  <K1 extends keyof T, K2 extends keyof U<T[K1]>>(path: [K1, K2]): (
     source: T,
-    path: [K1, K2],
-  ): Unset2<T, K1, K2>
+  ) => FpUnset2<T, K1, K2>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>
   >(
-    source: T,
     path: [K1, K2, K3],
-  ): Unset3<T, K1, K2, K3>
+  ): (source: T) => FpUnset3<T, K1, K2, K3>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>
   >(
-    source: T,
     path: [K1, K2, K3, K4],
-  ): Unset4<T, K1, K2, K3, K4>
+  ): (source: T) => FpUnset4<T, K1, K2, K3, K4>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>,
     K5 extends keyof U<U<U<U<T[K1]>[K2]>[K3]>[K4]>
   >(
-    source: T,
     path: [K1, K2, K3, K4, K5],
-  ): Unset5<T, K1, K2, K3, K4, K5>
-  <T, K1 extends keyof T>(source: Nullable<T>, path: [K1]): Nullable<
-    Unset1<T, K1>
-  >
-  <T, K1 extends keyof T, K2 extends keyof U<T[K1]>>(
+  ): (source: T) => FpUnset5<T, K1, K2, K3, K4, K5>
+
+  <K1 extends keyof T>(path: [K1]): (
     source: Nullable<T>,
-    path: [K1, K2],
-  ): Nullable<Unset2<T, K1, K2>>
+  ) => Nullable<FpUnset1<T, K1>>
+
+  <K1 extends keyof T, K2 extends keyof U<T[K1]>>(path: [K1, K2]): (
+    source: Nullable<T>,
+  ) => Nullable<FpUnset2<T, K1, K2>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>
   >(
-    source: Nullable<T>,
     path: [K1, K2, K3],
-  ): Nullable<Unset3<T, K1, K2, K3>>
+  ): (source: Nullable<T>) => Nullable<FpUnset3<T, K1, K2, K3>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>
   >(
-    source: Nullable<T>,
     path: [K1, K2, K3, K4],
-  ): Nullable<Unset4<T, K1, K2, K3, K4>>
+  ): (source: Nullable<T>) => Nullable<FpUnset4<T, K1, K2, K3, K4>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>,
     K5 extends keyof U<U<U<U<T[K1]>[K2]>[K3]>[K4]>
   >(
-    source: Nullable<T>,
     path: [K1, K2, K3, K4, K5],
-  ): Nullable<Unset5<T, K1, K2, K3, K4, K5>>
-  <T, K1 extends keyof T>(source: Undefinable<T>, path: [K1]): Undefinable<
-    Unset1<T, K1>
-  >
-  <T, K1 extends keyof T, K2 extends keyof U<T[K1]>>(
+  ): (source: Nullable<T>) => Nullable<FpUnset5<T, K1, K2, K3, K4, K5>>
+
+  <K1 extends keyof T>(path: [K1]): (
     source: Undefinable<T>,
-    path: [K1, K2],
-  ): Undefinable<Unset2<T, K1, K2>>
+  ) => Undefinable<FpUnset1<T, K1>>
+
+  <K1 extends keyof T, K2 extends keyof U<T[K1]>>(path: [K1, K2]): (
+    source: Undefinable<T>,
+  ) => Undefinable<FpUnset2<T, K1, K2>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>
   >(
-    source: Undefinable<T>,
     path: [K1, K2, K3],
-  ): Undefinable<Unset3<T, K1, K2, K3>>
+  ): (source: Undefinable<T>) => Undefinable<FpUnset3<T, K1, K2, K3>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>
   >(
-    source: Undefinable<T>,
     path: [K1, K2, K3, K4],
-  ): Undefinable<Unset4<T, K1, K2, K3, K4>>
+  ): (source: Undefinable<T>) => Undefinable<FpUnset4<T, K1, K2, K3, K4>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>,
     K5 extends keyof U<U<U<U<T[K1]>[K2]>[K3]>[K4]>
   >(
-    source: Undefinable<T>,
     path: [K1, K2, K3, K4, K5],
-  ): Undefinable<Unset5<T, K1, K2, K3, K4, K5>>
-  <T, K1 extends keyof T>(source: Optional<T>, path: [K1]): Optional<
-    Unset1<T, K1>
-  >
-  <T, K1 extends keyof T, K2 extends keyof U<T[K1]>>(
+  ): (source: Undefinable<T>) => Undefinable<FpUnset5<T, K1, K2, K3, K4, K5>>
+
+  <K1 extends keyof T>(path: [K1]): (
     source: Optional<T>,
-    path: [K1, K2],
-  ): Optional<Unset2<T, K1, K2>>
+  ) => Optional<FpUnset1<T, K1>>
+
+  <K1 extends keyof T, K2 extends keyof U<T[K1]>>(path: [K1, K2]): (
+    source: Optional<T>,
+  ) => Optional<FpUnset2<T, K1, K2>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>
   >(
-    source: Optional<T>,
     path: [K1, K2, K3],
-  ): Optional<Unset3<T, K1, K2, K3>>
+  ): (source: Optional<T>) => Optional<FpUnset3<T, K1, K2, K3>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>
   >(
-    source: Optional<T>,
     path: [K1, K2, K3, K4],
-  ): Optional<Unset4<T, K1, K2, K3, K4>>
+  ): (source: Optional<T>) => Optional<FpUnset4<T, K1, K2, K3, K4>>
+
   <
-    T,
     K1 extends keyof T,
     K2 extends keyof U<T[K1]>,
     K3 extends keyof U<U<T[K1]>[K2]>,
     K4 extends keyof U<U<U<T[K1]>[K2]>[K3]>,
     K5 extends keyof U<U<U<U<T[K1]>[K2]>[K3]>[K4]>
   >(
-    source: Optional<T>,
     path: [K1, K2, K3, K4, K5],
-  ): Optional<Unset5<T, K1, K2, K3, K4, K5>>
+  ): (source: Optional<T>) => Optional<FpUnset5<T, K1, K2, K3, K4, K5>>
 }
 
 // NOTE: use private implementation because typedoc generates wrong documentation.
-const unsetImplementation: UnsetFn = baseUnset
+const fpUnsetImplementation = <T>(): FpUnsetFnReturn<T> => (path: any[]) => (
+  source: any,
+) => baseUnset(source, path)
 
 /**
  * Removes the value on the specified path in source value. If the value is an array, the behavior
@@ -187,6 +180,6 @@ const unsetImplementation: UnsetFn = baseUnset
  * @param path path array of the nested value in the source
  * @returns source value with removed value
  */
-export const unset = unsetImplementation
+export const fpUnset = fpUnsetImplementation
 
-export default unset
+export default fpUnset
